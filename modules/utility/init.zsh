af2316481c18eb20cd0effad41bdaf5a4197227f
Steven Ringo <git@stevenringo.com>
2014-02-02 14:56:46 +1100

    Add `lb` as `ls -1` for bare directory listing

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -102,6 +102,7 @@ alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+alias lb='ls -1'         # Bare (filename and directories only)
 alias sl='ls'            # I often screw this up.
 
 # Grep
